aee84f19b3e3c5d051c326f9a4a80a8aa2725e95
matthewcardano <mcardano@loyola.edu>
2021-09-28 12:54:04 -0400

    Initial commit

---
+++ programmingAssignment01.py
@@ -0,0 +1,16 @@
+#Team Members: Matt Cardano
+#Course: CS 151, Prof. Mehri
+#Date: 9/27/21
+#Programming Assignment: 1
+#Program Inputs: asks the user for the dimensions (length, width, and height) of a room in feet
+#Program Outputs: computes the total area of the four walls and ceiling and determines the amount of primer and paint in gallons necessary to cover them
+
+length = int(input("enter length: "))
+width = int(input("enter width: "))
+height = int(input("enter height: "))
+area = (length * width * height) * 5
+primer = area / 200
+paint = area / 350
+print("area: ", area, "ft**2")
+print("primer: ", primer, "gallons")
+print("paint: ", paint, "gallons")
